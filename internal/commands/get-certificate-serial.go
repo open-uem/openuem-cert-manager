d3bee89908abecae0c1779b6c444621bc2ed2f59
Miguel Cabrerizo <doncicuto@gmail.com>
2024-11-04 12:46:43 +0100

    feat: get certificate serial number to use in installer

---
+++ internal/commands/get-certificate-serial.go
@@ -0,0 +1,38 @@
+package commands
+
+import (
+	"encoding/hex"
+	"fmt"
+
+	"github.com/doncicuto/openuem_utils"
+	"github.com/urfave/cli/v2"
+)
+
+func GetCertificateSerial() *cli.Command {
+	return &cli.Command{
+		Name:   "get-certificate-serial",
+		Usage:  "Get a certificate serial number",
+		Action: getCertificateSerial,
+		Flags:  getCertificateSerialFlags(),
+	}
+}
+
+func getCertificateSerialFlags() []cli.Flag {
+	return []cli.Flag{
+		&cli.StringFlag{
+			Name:     "path",
+			Usage:    "the path to the certificate",
+			Required: true,
+		},
+	}
+}
+
+func getCertificateSerial(cCtx *cli.Context) error {
+	cert, err := openuem_utils.ReadPEMCertificate(cCtx.String("path"))
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(hex.EncodeToString(cert.SerialNumber.Bytes()))
+	return nil
+}
